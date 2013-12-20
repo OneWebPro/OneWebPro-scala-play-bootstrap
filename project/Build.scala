9b61df1fdc652807b4157fa986a9921be1cc8f8f
mromanski <MRomanski@wp-sa.pl>
2013-12-20 09:06:36 +0100

    Initial commit.

---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "OneWebPro-scala-play-bootstrap"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "com.typesafe.slick" %% "slick" % "1.0.0",
+    "com.typesafe.play" %% "play-slick" % "0.5.0.8"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+  )
+
+}
