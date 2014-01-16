20e541524d5759db32cbe6334897a94b7c56a050
loki <maciej.romanski@.pl>
2014-01-16 19:17:15 +0100

    Add tests trait.

---
+++ test/pl/onewebpro/tests/GlobalTests.scala
@@ -0,0 +1,38 @@
+package pl.onewebpro.tests
+
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.db.slick.DB
+import scala.slick.jdbc.meta.MTable
+import play.api.Play.current
+import play.api.db.slick.Config.driver.simple._
+import scala.slick.session.Session
+import org.specs2.execute.AsResult
+
+
+trait GlobalTests {
+
+	def runSession[T](t: (Session) => T): T = {
+		running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+			DB.withSession {
+				implicit s: Session =>
+					t(s)
+			}
+		}
+	}
+
+	def run[T](t: => T): T = {
+		running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+			t
+		}
+	}
+
+  abstract class WithApp extends WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+    override def around[T: AsResult](t: => T): org.specs2.execute.Result = super.around {
+      t
+    }
+  }
+
+
+}
